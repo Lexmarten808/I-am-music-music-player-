c32fc7e715c664d068a0d21cf092a19490e4667a
Brandon Lasprilla <brandon.lasprilla@correounivalle.edu.co>
2026-01-16 17:17:32 -0500

    small fixes

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 
-config.resolver.sourceExts.push('mjs');
+config.resolver.sourceExts.push('mjs', 'cjs');
 
 module.exports = config;
